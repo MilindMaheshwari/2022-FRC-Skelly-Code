583f5b05f52a02d2f77ec21aab09dd15899ed0c9
MilindMaheshwari <maheshwarim297@gmail.com>
2022-01-22 13:16:29 -0500

    Changed conveyer to shooter in motor names to avoid confusion

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -11,18 +11,18 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Shooter extends SubsystemBase{
 
-    CANSparkMax firstConveyerMotor = new CANSparkMax(Constants.ShooterConsts.CAN_ADDRESS_FIRST_CONVEYER_MOTOR, MotorType.kBrushless);
-    CANSparkMax secondConveyerMotor = new CANSparkMax(Constants.ShooterConsts.CAN_ADDRESS_SECOND_CONVEYER_MOTOR, MotorType.kBrushless);
-    CANSparkMax thirdConveyerMotor = new CANSparkMax(Constants.ShooterConsts.CAN_ADDRESS_THIRD_CONVEYER_MOTOR, MotorType.kBrushless);
+    CANSparkMax firstShooterMotor = new CANSparkMax(Constants.ShooterConsts.CAN_ADDRESS_FIRST_SHOOTER_MOTOR, MotorType.kBrushless);
+    CANSparkMax secondShooterMotor = new CANSparkMax(Constants.ShooterConsts.CAN_ADDRESS_SECOND_SHOOTER_MOTOR, MotorType.kBrushless);
+    CANSparkMax thirdShooterMotor = new CANSparkMax(Constants.ShooterConsts.CAN_ADDRESS_THIRD_SHOOTER_MOTOR, MotorType.kBrushless);
     
     public Shooter(){
         super();
-        secondConveyerMotor.follow(firstConveyerMotor);
-        thirdConveyerMotor.follow(firstConveyerMotor);
+        secondShooterMotor.follow(firstShooterMotor);
+        thirdShooterMotor.follow(firstShooterMotor);
     }
 
     public void launch(int speed){
-        firstConveyerMotor.set(speed);
+        firstShooterMotor.set(speed);
     }
 }
 
